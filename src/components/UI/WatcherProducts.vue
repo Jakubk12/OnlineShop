59069ba663721c2b85a4851ae8b0efb6a57c3189
Jakubk12 <jakubkeska11@gmail.com>
2023-01-09 13:02:11 +0100

    added simple filter

--- src/components/UI/WatcherProducts.vue
+++ src/components/UI/WatcherProducts.vue
@@ -1,20 +1,8 @@
 <template>
   <div class="products">
-    <basic-button
-      @click="filter('premium')"
-      :class="{ selected: filters === 'premium' }"
-      >Premium</basic-button
-    >
-    <basic-button
-      @click="filter('middle')"
-      :class="{ selected: filters === 'middle' }"
-      >Middle</basic-button
-    >
-    <basic-button
-      @click="filter('economy')"
-      :class="{ selected: filters === 'economy' }"
-      >Economy</basic-button
-    >
+    <basic-button @click="filterPremium">Premium</basic-button>
+    <basic-button @click="filterMiddle">Middle</basic-button>
+    <basic-button @click="filterEconomic">Economy</basic-button>
   </div>
   <div class="inner-container">
     <products-component
@@ -57,90 +45,121 @@ export default {
   components: {
     ProductsComponent,
   },
-  data() {
-    return {
-      displayedProducts: [
-        {
-          id: "0",
-          name: "Delbana Fiorentino 41601.682.6.062",
-          type: "swiss",
-          glass: "sapphire",
-          price: 240,
-          destiny: "men",
-          description: "Elegance classic Watch with black belt",
-          image:
-            "https://dolinski.pl/images/ab__webp/thumbnails/600/800/detailed/1/Delbana-Florentino-416016826062_jpg.webp",
-        },
-        //Hugo Boss Hero 1513755
-        {
-          id: "1",
-          type: "swiss",
-          name: "Hugo Boss Hero 1513755",
-          glass: "saphire",
-          price: 360,
-          destiny: "men",
-          description: "An elegance and endurant silver watch for men",
-        },
-        {
-          id: "2",
-          type: "quartz",
-          name: "HUGO BOSS VIEW",
-          glass: "saphire",
-          price: 500,
-          destiny: "men",
-          description:
-            "A sport watch for men with waterproof until 5 atmospheres",
-        },
-        {
-          id: "3",
-          type: "quartz",
-          name: "Citizen Elegance BI5070-57L",
-          glass: "mineral",
-          price: 90,
-          destiny: "men",
-          description:
-            "Watch for men with high endurant and good taste with precisious quartz mechanism",
-        },
-        {
-          id: "4",
-          type: "quartz",
-          name: "Doxa D-Chrono 165.10.021.10",
-          glass: "sapphire",
-          price: 400,
-          destiny: "men",
-          description:
-            "An casual men watch  with classic character and silver belt, waterproof until 5 meters",
-        },
-        {
-          id: "5",
-          type: "automatic",
-          name: "Doxa SUB 200 Divingstar Automatic 799.10.361.31",
-          glass: "sapphire with antireflex",
-          price: 90,
-          destiny: "men",
-          description:
-            "Premium men watch with 200 meters waterproof. It keep your attention by unusual yellow color",
-        },
-      ],
-    };
-  },
+
   setup() {
+    const displayedProducts = ref([
+      {
+        id: "0",
+        name: "Delbana Fiorentino 41601.682.6.062",
+        type: "swiss",
+        glass: "sapphire",
+        price: 240,
+        destiny: "men",
+        description: "Elegance classic Watch with black belt",
+        image:
+          "https://dolinski.pl/images/ab__webp/thumbnails/600/800/detailed/1/Delbana-Florentino-416016826062_jpg.webp",
+      },
+      //Hugo Boss Hero 1513755
+      {
+        id: "1",
+        type: "swiss",
+        name: "Hugo Boss Hero 1513755",
+        glass: "saphire",
+        price: 360,
+        destiny: "men",
+        description: "An elegance and endurant silver watch for men",
+      },
+      {
+        id: "2",
+        type: "quartz",
+        name: "HUGO BOSS VIEW",
+        glass: "saphire",
+        price: 500,
+        destiny: "men",
+        description:
+          "A sport watch for men with waterproof until 5 atmospheres",
+      },
+      {
+        id: "3",
+        type: "quartz",
+        name: "Citizen Elegance BI5070-57L",
+        glass: "mineral",
+        price: 90,
+        destiny: "men",
+        description:
+          "Watch for men with high endurant and good taste with precisious quartz mechanism",
+      },
+      {
+        id: "4",
+        type: "quartz",
+        name: "Doxa D-Chrono 165.10.021.10",
+        glass: "sapphire",
+        price: 400,
+        destiny: "men",
+        description:
+          "An casual men watch  with classic character and silver belt, waterproof until 5 meters",
+      },
+      {
+        id: "5",
+        type: "automatic",
+        name: "Doxa SUB 200 Divingstar Automatic 799.10.361.31",
+        glass: "sapphire with antireflex",
+        price: 90,
+        destiny: "men",
+        description:
+          "Premium men watch with 200 meters waterproof. It keep your attention by unusual yellow color",
+      },
+    ]);
     const filters = ref(null);
-    const expectedProducts = computed(function () {
-      displayedProducts.value = displayedProducts.value.filter((prd1) => {
-        if (prd1.price < 250) {
-          return prd1.price < 250 && filters.value === "economy";
-        } else if (
-          prd1.price >= 250 &&
-          prd1.price <= 1000 &&
-          filters.value === "middle"
-        ) {
-          return prd1.price >= 250 && prd1.price <= 1000;
-        } else if (prd1.price >= 1000) {
-          return prd1.price >= 1000 && filters.value === "premium";
-        }
-      });
-    });
+    // const expectedProducts = computed(function () {
+    //   if (!filters.value) {
+    //     return expectedProducts;
+    //   }
+    //   return (displayedProducts.value = displayedProducts.value.filter(
+    //     (prd1) => {
+    //       if (prd1.price < 250) {
+    //         return prd1.price < 250 && filters.value === "economy";
+    //       } else if (
+    //         prd1.price >= 250 &&
+    //         prd1.price <= 1000 &&
+    //         filters.value === "middle"
+    //       ) {
+    //         return prd1.price >= 250 && prd1.price <= 1000;
+    //       } else if (prd1.price >= 1000) {
+    //         return prd1.price >= 1000 && filters.value === "premium";
+    //       }
+    //     }
+    //   ));
+    // });
+    function filterEconomic() {
+      displayedProducts.value = displayedProducts.value.filter(
+        (prd) => prd.price <= 250
+      );
+    }
+    function filterMiddle() {
+      displayedProducts.value = displayedProducts.value.filter(
+        (prd) => prd.price >= 250 && prd.price <= 1000
+      );
+    }
+    function filterPremium() {
+      displayedProducts.value = displayedProducts.value.filter(
+        (prd) => prd.price >= 1000
+      );
+    }
+    // function expectedProducts() {
+    //   displayedProducts.value = displayedProducts.value.filter((prd1) => {
+    //     if (prd1.price < 250) {
+    //       return prd1.price < 250 && filters.value === "economy";
+    //     } else if (prd1.price > 1000) {
+    //       return prd1.price > 1000 && filters.value === "premium";
+    //     } else {
+    //       prd1.price > 250 && prd1.price < 1000;
+    //       return (
+    //         prd1.price > 250 && prd1.price < 1000 && filters.value === "middle"
+    //       );
+    //     }
+    //   });
+    // }
     function filter(mode) {
       filters.value = mode;
     }
@@ -199,7 +218,10 @@ export default {
     return {
       filter,
       filters,
-      expectedProducts,
+      filterEconomic,
+      filterMiddle,
+      filterPremium,
+      displayedProducts,
     };
   },
 
