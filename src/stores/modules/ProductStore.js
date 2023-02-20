290fddb4cbd588c6d27430d4811eb14aab34df7f
Jakubk12 <jakubkeska11@gmail.com>
2023-02-20 16:29:09 +0100

    changing of files to pinia

--- src/stores/modules/ProductStore.js
+++ src/stores/modules/ProductStore.js
@@ -3,7 +3,14 @@ import { defineStore } from "pinia";
 export const useProductStore = defineStore("ProductStore", {
   state: () => {
     return {
-      ProductStore,
+      products: [],
+      accesories: [],
     };
+  },
+  actions: {
+    async fillArray() {
+      this.products = (await import("@/items/products.json")).default;
+      this.accesories = (await import("@/items/accesories.json")).default;
+    },
   },
 });
